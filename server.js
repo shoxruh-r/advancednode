0de4a0e581cae09d1cbae62b1d0343035ac5140e
Shokhrukh Rustamaliev <black.doppi@gmail.com>
2022-06-08 20:04:00 +0500

    MongoDB added.

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
+
 require('dotenv').config();
+
 const express = require('express');
 const passport = require('passport')
 const session = require('express-session')
@@ -7,10 +9,13 @@ const myDB = require('./connection');
 const { ObjectId } = require('mongodb')
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
+
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+
 app.set('view engine', 'pug')
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -25,19 +30,33 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 
-passport.serializeUser((user, done) => {
-  done(null, user._id);
+
+myDB(async client => {
+  const myDataBase = await client.db('database').collection('users');
+
+  // Be sure to change the title
+  app.route('/').get((req, res) => {
+    // Change the response to render the Pug template
+    res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'Please login' })
+  });
+
+  // Serialization and deserialization here...
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc);
+    });
+  });
+  // Be sure to add this...
+}).catch(e => {
+  app.route('/').get((req, res) => {
+    res.render(process.cwd() + '/views/pug/index', { title: e, message: 'Unable to login' });
+  });
 });
 
-passport.deserializeUser((id, done) => {
-  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-  done(null, null);
-  // });
-});
-
-app.route('/').get((req, res) => {
-  res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'Please login' })
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
