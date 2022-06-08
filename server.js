827298cf5807e51d3e73537df5d993b0cf4a7f97
Shokhrukh Rustamaliev <black.doppi@gmail.com>
2022-06-08 11:54:13 +0500

    Passport

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 'use strict';
 require('dotenv').config();
 const express = require('express');
+const passport = require('passport')
+const session = require('express-session')
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
@@ -12,8 +14,18 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.route('/').get((req, res) => {
-  res.render('pug', { title: "Hello", message: "Pleaase login" });
+  res.render(process.cwd() + '/views/pug/index', { title: 'Hello', message: 'Please login' })
 });
 
 const PORT = process.env.PORT || 3000;
