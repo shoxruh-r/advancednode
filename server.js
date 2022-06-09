25b1c4f866fe8025e578e9c7196ae7e908474104
Shokhrukh Rustamaliev <black.doppi@gmail.com>
2022-06-09 11:23:24 +0500

    Register.

--- server.js
+++ server.js
@@ -61,14 +61,17 @@ const ensureAuthenticated = (req, res, next) => {
 
 
 myDB(async client => {
-  const myDataBase = await client.db('database').collection('users')
+  const myDataBase = await client
+    .db('database')
+    .collection('users')
 
   app.route('/')
     .get((req, res) => {
       res.render(process.cwd() + '/views/pug/index', {
         title: "Connected to Database",
         message: 'Please login',
-        showLogin: true
+        showLogin: true,
+        showRegistration: true
       })
     })
 
@@ -84,7 +87,33 @@ myDB(async client => {
 
   app.route('/profile')
     .get(ensureAuthenticated, (req, res) => {
-      res.render(process.cwd() + '/views/pug/profile', { username: req.user.username })
+      res.render(process.cwd() + '/views/pug/profile', {
+        username: req.user.username
+      })
+    })
+
+  app.route('/register')
+    .post((req, res, next) => {
+      myDataBase.findOne({ username: req.body.username }, (err, user) => {
+        if (err)
+          next(err)
+        else if (user)
+          res.redirect('/')
+        else
+          myDataBase.insertOne({
+            username: req.body.username,
+            password: req.body.password
+          }, (err, doc) => {
+            if (err)
+              res.redirect('/')
+            else
+              // The inserted document is held within
+              // the ops property of the doc
+              next(null, doc.ops[0])
+          })
+      })
+    }, passport.authenticate('local', { failureRedirect: '/' }), (req, res, next) => {
+      res.redirect('/profile')
     })
 
   app.use((req, res, next) => {
