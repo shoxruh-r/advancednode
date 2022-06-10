319ce41404fbbcc394572b748a4c570ebdb208ad
Shokhrukh Rustamaliev <black.doppi@gmail.com>
2022-06-10 10:31:48 +0500

    GitHubStrategy

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
-const GithubStrategy = require('passport-github').Strategy
+const GitHubStrategy = require('passport-github').Strategy
 const { ObjectId } = require('mongodb')
 const bcrypt = require('bcrypt')
 
